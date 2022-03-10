1614b8c96cd5fdc6bb4d426f385cb432f2805f0b
muthioo-faith <muthioofaith@gmail.com>
2022-03-10 08:50:19 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+school()
+ var y=(1/2)
+  statement()
+    myname()
+}
+fun school(){
+    var name="CodeHive"
+    println(name[5].toString()+name[6]+name[7]+name[8])
+}
+fun output(a:Int,b:Int,){
+    var no1=10
+    var no2=5
+    println(a%b)
+}
+fun statement(){
+    var name="faith"
+    var  age= 20
+    println("hi my name is $name and am $age years old")
+}
+fun myname(){
+    var y="faith"
+    println(y.length)
+}
